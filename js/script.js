5584ccf8e9388e5d919e1fbea02026eb7f59e36d
MYKYTA RYSIEV <nickpolska21@gmail.com>
2021-06-12 15:45:05 +0200

    Added UA translation of project

---
+++ js/script.js
@@ -0,0 +1,29 @@
+{
+  const welcome = () => {
+    console.log("Hello to all developers, who visit here 🔒")
+  };
+  
+  const toggleBackground = () => {
+    const container = document.querySelector(".container")
+    const nextColorName = document.querySelector(".js-nextColorName");
+    const imageColor = document.querySelector(".imageColor");
+    const nameColor = document.querySelector(".nameColor");
+    const underlineColor = document.querySelector(".underlineColor");
+
+    container.classList.toggle("whiteBackground");
+    nextColorName.innerText = container.classList.contains("whiteBackground") ? "lightseagreen" : "white";
+    imageColor.classList.toggle("lightSeaGreenColorImage");
+    nameColor.classList.toggle("lightSkyBlueColorName");
+    underlineColor.classList.toggle("lightSeaGreenColorUnderline"); 
+
+  };
+  
+  const init = () => {
+    const changeBackground = document.querySelector(".js-changeBackground");
+    changeBackground.addEventListener("click", toggleBackground);
+    
+    welcome();
+ };
+
+ init();
+}
